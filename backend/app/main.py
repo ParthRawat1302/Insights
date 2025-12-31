c9c59478623ccb744367802b327478b2110f92b6
ParthRawat1302 <parthrawat0987654321@gmail.com>
2025-12-31 21:05:09 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://insights-frontend-c85m.onrender.com"],
+    allow_origins=["https://insights-frontend-v2n9.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
